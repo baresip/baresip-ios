8967c2476abec7a8aa8146e199b451227cec6b48
Vitaly Takmazov <vitalyster@gmail.com>
2014-06-05 10:06:09 +0400

    Find iPhone SDK tools via xcrun; replaced armv6 with arm64 arch; re-0.4.7, rem-0.4.6, baresip-0.4.10

--- mk/contrib.mk
+++ mk/contrib.mk
@@ -9,64 +9,40 @@
 # path to external source code
 #
 
-SOURCE_PATH	:= /Users/alfredh/src
+SOURCE_PATH	:= $(shell pwd)
 
-LIBRE_PATH	:= $(SOURCE_PATH)/re-0.4.2
-LIBREM_PATH	:= $(SOURCE_PATH)/rem-0.4.2
-BARESIP_PATH	:= $(SOURCE_PATH)/baresip-0.4.3
+LIBRE_PATH	:= $(SOURCE_PATH)/re-0.4.7
+LIBREM_PATH	:= $(SOURCE_PATH)/rem-0.4.6
+BARESIP_PATH	:= $(SOURCE_PATH)/baresip-0.4.10
 
 
 #
 # tools and SDK
 #
 
-ROOT_ARM	:= /Developer/Platforms/iPhoneOS.platform/Developer
-ROOT_SIM	:= /Developer/Platforms/iPhoneSimulator.platform/Developer
-
 # Auto-detect the latest SDK
 ifeq ($(SDK_VER),)
-SDK_VER   := $(shell [ -d $(ROOT_ARM)/SDKs/iPhoneOS5.1.sdk ] && echo "5.1")
-endif
-ifeq ($(SDK_VER),)
-SDK_VER   := $(shell [ -d $(ROOT_ARM)/SDKs/iPhoneOS4.3.sdk ] && echo "4.3")
-endif
-ifeq ($(SDK_VER),)
-SDK_VER   := $(shell [ -d $(ROOT_ARM)/SDKs/iPhoneOS4.2.sdk ] && echo "4.2")
-endif
-ifeq ($(SDK_VER),)
-SDK_VER   := $(shell [ -d $(ROOT_ARM)/SDKs/iPhoneOS4.1.sdk ] && echo "4.1")
-endif
-ifeq ($(SDK_VER),)
-SDK_VER   := $(shell [ -d $(ROOT_ARM)/SDKs/iPhoneOS4.0.sdk ] && echo "4.0")
+SDK_VER   := $(shell xcrun -find -sdk iphoneos --show-sdk-version)
 endif
 ifeq ($(SDK_VER),)
 $(warning no iPhone SDK detected)
 endif
 
-ifeq ($(SDK_VER),4.1)
-ARM_MACHINE := arm-apple-darwin10
-SIM_MACHINE := i686-apple-darwin10
-endif
-ifeq ($(SDK_VER),4.0)
-ARM_MACHINE := arm-apple-darwin10
-SIM_MACHINE := i686-apple-darwin10
-endif
-
 SDK_MAJOR	:= $(shell echo $(SDK_VER) | cut -d'.' -f 1)
 SDK_MINOR	:= $(shell echo $(SDK_VER) | cut -d'.' -f 2)
-SDK_ARM		:= $(ROOT_ARM)/SDKs/iPhoneOS$(SDK_VER).sdk
-SDK_SIM		:= $(ROOT_SIM)/SDKs/iPhoneSimulator$(SDK_VER).sdk
-CC_ARM		:= $(ROOT_ARM)/usr/bin/gcc
-CC_SIM		:= $(ROOT_SIM)/usr/bin/gcc
+SDK_ARM		:= $(shell xcrun -find -sdk iphoneos --show-sdk-path)
+SDK_SIM		:= $(shell xcrun -find -sdk iphonesimulator --show-sdk-path)
+CC_ARM		:= $(shell xcrun -find -sdk iphoneos gcc)
+CC_SIM		:= $(shell xcrun -find -sdk iphonesimulator gcc)
 
 CONTRIB_DIR	:= $(PWD)/contrib
-CONTRIB_ARMV6	:= $(CONTRIB_DIR)/armv6
+CONTRIB_AARCH64	:= $(CONTRIB_DIR)/aarch64
 CONTRIB_ARMV7	:= $(CONTRIB_DIR)/armv7
 CONTRIB_I386	:= $(CONTRIB_DIR)/i386
 CONTRIB_FAT	:= $(CONTRIB_DIR)/fat
 
 BUILD_DIR	:= $(PWD)/build
-BUILD_ARMV6	:= $(BUILD_DIR)/armv6
+BUILD_AARCH64	:= $(BUILD_DIR)/aarch64
 BUILD_ARMV7	:= $(BUILD_DIR)/armv7
 BUILD_I386	:= $(BUILD_DIR)/i386
 BUILD_FAT	:= $(BUILD_DIR)/fat
@@ -78,13 +54,13 @@ SIMROOT_ALT	:= $(CONTRIB_FAT)
 
 EXTRA_CFLAGS       := -DIPHONE -pipe -no-cpp-precomp -isysroot $(SDK_ARM)
 EXTRA_CFLAGS_SIM   := -DIPHONE -pipe -no-cpp-precomp -isysroot $(SDK_SIM)
-EXTRA_CFLAGS_ARMV6 := -arch armv6 -I$(CONTRIB_ARMV6)/include $(EXTRA_CFLAGS)
+EXTRA_CFLAGS_AARCH64 := -arch aarch64 -I$(CONTRIB_AARCH64)/include $(EXTRA_CFLAGS)
 EXTRA_CFLAGS_ARMV7 := -arch armv7 -I$(CONTRIB_ARMV7)/include $(EXTRA_CFLAGS)
 EXTRA_CFLAGS_I386  := -arch i386 -I$(CONTRIB_I386)/include $(EXTRA_CFLAGS_SIM)
 
 EXTRA_LFLAGS       := -L$(CONTRIB_FAT)/lib -isysroot $(SDK_ARM)
 EXTRA_LFLAGS_SIM   := -L$(CONTRIB_FAT)/lib -isysroot $(SDK_SIM)
-EXTRA_LFLAGS_ARMV6 := -arch armv6 -L$(CONTRIB_ARMV6)/lib $(EXTRA_LFLAGS)
+EXTRA_LFLAGS_AARCH64 := -arch aarch64 -L$(CONTRIB_AARCH64)/lib $(EXTRA_LFLAGS)
 EXTRA_LFLAGS_ARMV7 := -arch armv7 -L$(CONTRIB_ARMV7)/lib $(EXTRA_LFLAGS)
 EXTRA_LFLAGS_I386  := -arch i386 -L$(CONTRIB_I386)/lib $(EXTRA_LFLAGS_SIM)
 
@@ -92,7 +68,7 @@ EXTRA_LFLAGS_I386  := -arch i386 -L$(CONTRIB_I386)/lib $(EXTRA_LFLAGS_SIM)
 EXTRA_I386      := \
 	EXTRA_CFLAGS='-D__DARWIN_ONLY_UNIX_CONFORMANCE \
 		-D__IPHONE_OS_VERSION_MIN_REQUIRED=30000 \
-		-mmacosx-version-min=10.5 \
+		-miphoneos-version-min=5.1 \
 		-Wno-cast-align -Wno-shorten-64-to-32 \
 		-Wno-aggregate-return \
 		-arch i386 \
@@ -100,20 +76,20 @@ EXTRA_I386      := \
 		-I$(CONTRIB_I386)/include \
 		-I$(CONTRIB_I386)/include/rem' \
 	OBJCFLAGS='-fobjc-abi-version=2 -fobjc-legacy-dispatch' \
-	EXTRA_LFLAGS='-arch i386 -L$(CONTRIB_FAT)/lib \
+	EXTRA_LFLAGS='-miphoneos-version-min=5.1 -arch i386 -L$(CONTRIB_FAT)/lib \
 		-isysroot $(SDK_SIM)'
 
-EXTRA_ARMV6       := \
-	EXTRA_CFLAGS='-arch armv6 \
-		-I$(CONTRIB_ARMV6)/include \
-		-I$(CONTRIB_ARMV6)/include/rem \
+EXTRA_AARCH64       := \
+	EXTRA_CFLAGS='-arch arm64 \
+		-I$(CONTRIB_AARCH64)/include \
+		-I$(CONTRIB_AARCH64)/include/rem \
 		-Wno-cast-align -Wno-shorten-64-to-32 \
 		-Wno-aggregate-return \
-		-isysroot $(SDK_ARM) -DHAVE_ARMV6' \
-	EXTRA_LFLAGS='-arch armv6 -mcpu=arm1176jzf-s -marm \
+		-isysroot $(SDK_ARM) -DHAVE_AARCH64' \
+	EXTRA_LFLAGS='-arch arm64 -mcpu=generic -marm \
 		-L$(CONTRIB_FAT)/lib -isysroot $(SDK_ARM)' \
-	OS=darwin ARCH=armv6 CROSS_COMPILE=$(ARM_MACHINE) \
-	HAVE_ARMV6=1
+	OS=darwin ARCH=arm64 CROSS_COMPILE=$(ARM_MACHINE) \
+	HAVE_ARM64=1
 
 EXTRA_ARMV7       := \
 	EXTRA_CFLAGS='-arch armv7 \
@@ -136,7 +112,7 @@ EXTRA_ARMV7       := \
 contrib:	baresip
 
 
-$(BUILD_ARMV6) $(BUILD_ARMV7) $(BUILD_I386) $(BUILD_FAT):
+$(BUILD_AARCH64) $(BUILD_ARMV7) $(BUILD_I386) $(BUILD_FAT):
 	@mkdir -p $@
 
 $(CONTRIB_FAT) $(CONTRIB_FAT)/lib:
@@ -153,10 +129,10 @@ LIBRE_BUILD_FLAGS := \
 libre: $(CONTRIB_FAT)/lib
 	@rm -f $(LIBRE_PATH)/libre.*
 	@make -sC $(LIBRE_PATH) CC='$(CC_ARM)' \
-		BUILD=$(BUILD_ARMV6)/libre \
+		BUILD=$(BUILD_AARCH64)/libre \
 		SYSROOT=$(ARMROOT) SYSROOT_ALT=$(ARMROOT_ALT) \
-		$(LIBRE_BUILD_FLAGS) $(EXTRA_ARMV6) \
-		PREFIX= DESTDIR=$(CONTRIB_ARMV6) \
+		$(LIBRE_BUILD_FLAGS) $(EXTRA_AARCH64) \
+		PREFIX= DESTDIR=$(CONTRIB_AARCH64) \
 		all install
 
 	@rm -f $(LIBRE_PATH)/libre.*
@@ -179,7 +155,7 @@ libre: $(CONTRIB_FAT)/lib
 
 	@lipo \
 		-arch i386 $(CONTRIB_I386)/lib/libre.a \
-		-arch armv6 $(CONTRIB_ARMV6)/lib/libre.a \
+		-arch arm64 $(CONTRIB_AARCH64)/lib/libre.a \
 		-arch armv7 $(CONTRIB_ARMV7)/lib/libre.a \
 		-create -output $(CONTRIB_FAT)/lib/libre.a
 
@@ -194,10 +170,10 @@ LIBREM_BUILD_FLAGS := \
 librem: libre
 	@rm -f $(LIBREM_PATH)/librem.*
 	@make -sC $(LIBREM_PATH) CC='$(CC_ARM)' \
-		BUILD=$(BUILD_ARMV6)/librem \
+		BUILD=$(BUILD_AARCH64)/librem \
 		SYSROOT=$(ARMROOT) SYSROOT_ALT=$(ARMROOT_ALT) \
-		$(LIBREM_BUILD_FLAGS) $(EXTRA_ARMV6) \
-		PREFIX= DESTDIR=$(CONTRIB_ARMV6) \
+		$(LIBREM_BUILD_FLAGS) $(EXTRA_AARCH64) \
+		PREFIX= DESTDIR=$(CONTRIB_AARCH64) \
 		all install
 
 	@rm -f $(LIBREM_PATH)/librem.*
@@ -220,7 +196,7 @@ librem: libre
 
 	@lipo \
 		-arch i386 $(CONTRIB_I386)/lib/librem.a \
-		-arch armv6 $(CONTRIB_ARMV6)/lib/librem.a \
+		-arch arm64 $(CONTRIB_AARCH64)/lib/librem.a \
 		-arch armv7 $(CONTRIB_ARMV7)/lib/librem.a \
 		-create -output $(CONTRIB_FAT)/lib/librem.a
 
@@ -239,7 +215,7 @@ BARESIP_BUILD_FLAGS_I386 := \
 	$(BARESIP_BUILD_FLAGS) \
 	EXTRA_MODULES='g711 audiounit avcapture opengles'
 
-BARESIP_BUILD_FLAGS_ARMV6 := \
+BARESIP_BUILD_FLAGS_AARCH64 := \
 	$(BARESIP_BUILD_FLAGS) \
 	EXTRA_MODULES='g711 audiounit avcapture opengles'
 
@@ -251,10 +227,10 @@ BARESIP_BUILD_FLAGS_ARMV7 := \
 baresip: librem libre
 	@rm -f $(BARESIP_PATH)/src/static.c ../baresip/libbaresip.*
 	@make -sC $(BARESIP_PATH) CC='$(CC_ARM)' \
-		BUILD=$(BUILD_ARMV6)/baresip \
+		BUILD=$(BUILD_AARCH64)/baresip \
 		SYSROOT=$(ARMROOT) SYSROOT_ALT=$(ARMROOT_ALT) \
-		$(BARESIP_BUILD_FLAGS_ARMV6) $(EXTRA_ARMV6) \
-		PREFIX= DESTDIR=$(CONTRIB_ARMV6) \
+		$(BARESIP_BUILD_FLAGS_AARCH64) $(EXTRA_AARCH64) \
+		PREFIX= DESTDIR=$(CONTRIB_AARCH64) \
 		install-static
 
 	@rm -f $(BARESIP_PATH)/src/static.c ../baresip/libbaresip.*
@@ -275,6 +251,6 @@ baresip: librem libre
 
 	@lipo \
 		-arch i386 $(CONTRIB_I386)/lib/libbaresip.a \
-		-arch armv6 $(CONTRIB_ARMV6)/lib/libbaresip.a \
+		-arch arm64 $(CONTRIB_AARCH64)/lib/libbaresip.a \
 		-arch armv7 $(CONTRIB_ARMV7)/lib/libbaresip.a \
 		-create -output $(CONTRIB_FAT)/lib/libbaresip.a
